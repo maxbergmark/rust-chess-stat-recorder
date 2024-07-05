b8e8c35147865b56d50132599b36441499575e40
Max Bergmark <max.bergmark@gmail.com>
2024-07-05 21:22:34 +0200

    Add lints

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
 #![warn(
+    // missing_docs,
+    // unreachable_pub,
     keyword_idents,
     missing_copy_implementations,
     missing_debug_implementations,
@@ -9,7 +11,20 @@
     unused_import_braces,
     future_incompatible,
     nonstandard_style,
-    // rust_2018_idioms,
+    bad_style,
+    dead_code,
+    improper_ctypes,
+    non_shorthand_field_patterns,
+    no_mangle_generic_items,
+    overflowing_literals,
+    path_statements,
+    patterns_in_fns_without_body,
+    unconditional_recursion,
+    unused,
+    unused_allocation,
+    unused_comparisons,
+    unused_parens,
+    while_true,
 )]
 
 use file_util::{from_file, to_local_filename, write_batch};
